df2dbd4d1030deb1c60ab16269523bbdd40ebe10
iamangelu <67723309+iamangelu@users.noreply.github.com>
2020-08-01 17:55:20 +0800

    adding files

---
+++ src/robots.js
@@ -0,0 +1,92 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Thor Odinson',
+	username: 'thor',
+	email: 'thor@avenger.com'
+},
+{
+	id: 2,
+	name: 'Tony Stark',
+	username: 'stark',
+	email: 'ironman@avenger.com'
+},
+{
+	id: 3,
+	name: 'Robert Banner',
+	username: 'hulk',
+	email: 'hulk@avenger.com'
+},
+{
+	id: 4,
+	name: 'Steven Rogers',
+	username: 'steve',
+	email: 'captain.america@avenger.com'
+},
+{
+	id: 5,
+	name: 'Clinton Barton',
+	username: 'hawkeye',
+	email: 'hawkeye@avenger.com'
+},
+{
+	id: 6,
+	name: 'Wanda Maximoff',
+	username: 'wanda',
+	email: 'scarletwitch@avenger.com'
+},
+{
+	id: 7,
+	name: 'Harry Cleese',
+	username: 'hercules',
+	email: 'hercules@avenger.com'
+},
+{
+	id: 8,
+	name: 'Luke Charles',
+	username: 'blackpanther',
+	email: 'blackpanther@avenger.com'
+},
+{
+	id: 9,
+	name: 'Dane Whitman',
+	username: 'blackknight',
+	email: 'blackknight@avenger.com'
+},
+{
+	id: 10,
+	name: 'James Howlett',
+	username: 'logan',
+	email: 'wolverine@avenger.com'
+},
+{
+	id: 11,
+	name: 'Jack Hart',
+	username: 'jack',
+	email: 'jackofhearts@avenger.com'
+},
+{
+	id: 12,
+	name: 'Mandy Celestine',
+	username: 'mantis',
+	email: 'mantis@avenger.com'
+},
+{
+	id: 13,
+	name: 'Henry McCoy',
+	username: 'beast',
+	email: 'beast@avenger.com'
+},
+{
+	id: 14,
+	name: 'Peter Parker',
+	username: 'spiderman',
+	email: 'spiderman@avenger.com'
+},
+{
+	id: 15,
+	name: 'William Baker',
+	username: 'sandman',
+	email: 'sandman@avenger.com'
+}
+]
